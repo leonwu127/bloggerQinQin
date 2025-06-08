0d207e608c3c7f34585561101f3dd1af9e231018
leonwu127 <leonwu127@gmail.com>
2025-06-08 23:17:18 +0100

    Enhance profile design with modern styling (#2)

--- src/data/profile.js
+++ src/data/profile.js
@@ -3,8 +3,10 @@ export const profile = {
   email: "leonwu127@gmail.com",
   phone: "+447784369154",
   title: "Senior Software Developer",
-  description: `I am a passionate Senior Software Developer highly skilled in backend technologies 
+  github: "https://github.com/leonwu127",
+  linkedin: "https://www.linkedin.com/in/zhiqingwu/",
+  description: `I am a passionate Senior Software Developer highly skilled in backend technologies
   and Agile methodologies, very professional in game system development, Web 3 and Fintech.
-  In my current job, I am working closely with management and business leaders, sharing knowledge 
-  and delivering high quality backend services (SLA targeting 99.99%).`
-}; 
+  In my current job, I am working closely with management and business leaders, sharing knowledge
+  and delivering high quality backend services (SLA targeting 99.99%).`,
+};
